d7e981e4e93ee47c3f358e5a21a8df8181bca642
averycheungcl <averycheungcl@gmail.com>
2025-09-27 23:27:48 +0800

    added cmake and fix rpiservo

--- rpiservo.py
+++ rpiservo.py
@@ -2,25 +2,27 @@ import lgpio
 import time
 
 # Open the GPIO chip
-h = lgpio.gpiochip_open(0)
+h = lgpio.gpiochip_open(4)
 
 servo_pin = 12
 
-# Set up PWM at 50 Hz
-lgpio.tx_pwm(h, servo_pin, 50, 7.5)  # 7.5% duty = ~1.5 ms pulse (center)
+lgpio.gpio_claim_output(h,servo_pin)
+print('opened chip')
 
+# Set up PWM at 50 Hz
+try:
+    lgpio.tx_pwm(h, servo_pin, 50, 2.5)  # 7.5% duty = ~1.5 ms pulse (center)
+    print('first worked')
+except Exception as e:
+    print(f"error:{e}")
 time.sleep(2)
 
 # Move to 0 degrees (~1 ms pulse, 5% duty)
-lgpio.tx_pwm(h, servo_pin, 50, 5.0)
+lgpio.tx_pwm(h, servo_pin, 50, 7.5)
 time.sleep(2)
 
 # Move to 180 degrees (~2 ms pulse, 10% duty)
-lgpio.tx_pwm(h, servo_pin, 50, 10.0)
-time.sleep(2)
-
-# Back to center
-lgpio.tx_pwm(h, servo_pin, 50, 7.5)
+lgpio.tx_pwm(h, servo_pin, 50, 12.5)
 time.sleep(2)
 
 # Stop PWM output
